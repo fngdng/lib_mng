0cb7358e78b41f5301525e5d9428e5cde7f54a47
fngdng <aduangfongdz@outlook.com>
2024-05-14 21:24:57 +0700

    4

--- library_management/settings.py
+++ library_management/settings.py
@@ -122,6 +122,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / STATIC_URL
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
